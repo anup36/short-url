307114290911f08ee40ac05550da7ca674d18c75
anuppanwar36 <anuppanwar36@gmail.com>
2021-03-14 14:42:29 +0530

    mongdob url

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodb: {
-    host: 'localhost',
+    host: 'mongodb+srv://urfeedMongo:d7B07zCte8bICmpL@cluster0.cwzx2.mongodb.net/urls?retryWrites=true&w=majority',
     port: 27017,
     database: 'shortener',
     username: 'root',
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('./env');
 
-mongoose.connect(`mongodb://${config.mongodb.host}/${config.mongodb.database}`, { reconnectTries: 5 })
+mongoose.connect(config.mongodb.host, { reconnectTries: 5 })
   .then(() => {
     // eslint-disable-next-line no-console
     console.log('mongo connected');
